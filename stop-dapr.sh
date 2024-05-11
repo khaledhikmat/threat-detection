208d8d3815df3c506253fc5dd07ccd16916bc7ed
Khaled Hikmat <khaled.hikmat@gmail.com>
2024-05-11 15:50:27 -0500

    Fixed stop scripts

--- stop-dapr.sh
+++ stop-dapr.sh
@@ -1,7 +1,7 @@
 unset THREAT_DETECTION_MODE && dapr stop -f . &&
-(lsof -i:8080 | grep main) | awk '{print $2}' | xargs kill &&
-(lsof -i:8081 | grep main) | awk '{print $2}' | xargs kill &&
-(lsof -i:8082 | grep main) | awk '{print $2}' | xargs kill &&
-(lsof -i:8083 | grep main) | awk '{print $2}' | xargs kill &&
-(lsof -i:3000 | grep main) | awk '{print $2}' | xargs kill
+lsof -i:8080 | grep main | awk '{print $2}' | xargs kill &&
+lsof -i:8081 | grep main | awk '{print $2}' | xargs kill &&
+lsof -i:8082 | grep main | awk '{print $2}' | xargs kill &&
+lsof -i:8083 | grep main | awk '{print $2}' | xargs kill &&
+lsof -i:3000 | grep main | awk '{print $2}' | xargs kill
 
--- stop-diagrid.sh
+++ stop-diagrid.sh
@@ -1,8 +1,8 @@
 unset THREAT_DETECTION_MODE && 
 diagrid dev stop -f dev-eagle-threat-detection.yaml &&
-(lsof -i:8080 | grep main) | awk '{print $2}' | xargs kill &&
-(lsof -i:8081 | grep main) | awk '{print $2}' | xargs kill &&
-(lsof -i:8082 | grep main) | awk '{print $2}' | xargs kill &&
-(lsof -i:8083 | grep main) | awk '{print $2}' | xargs kill &&
-(lsof -i:3000 | grep main) | awk '{print $2}' | xargs kill
+lsof -i:8080 | grep main | awk '{print $2}' | xargs kill &&
+lsof -i:8081 | grep main | awk '{print $2}' | xargs kill &&
+lsof -i:8082 | grep main | awk '{print $2}' | xargs kill &&
+lsof -i:8083 | grep main | awk '{print $2}' | xargs kill &&
+lsof -i:3000 | grep main | awk '{print $2}' | xargs kill
 
